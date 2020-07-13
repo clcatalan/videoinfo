a16a3cf2c56d077369bebdf07f5b6976aebb8290
Carlos Catalan <crlsctalan23@gmail.com>
2020-07-13 21:13:40 +0800

    updated with test styles

--- index.js
+++ index.js
@@ -4,17 +4,18 @@
 //all code related to creating windows and handling the electron side will be placed here
 //restart electron from terminal if changes are made here, if on other files, just click command+r
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg')
 
 //overall running process
 const {app, BrowserWindow, ipcMain} = electron
-
+let mainWindow
 
 //electron on ready event
 app.on('ready', () => {
     console.log('app is now ready')
 
     //creates new window
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true
           }
@@ -22,6 +23,10 @@ app.on('ready', () => {
     mainWindow.loadURL(`file://${__dirname}/index.html`)
 })
 
-ipcMain.on('video:submit', (path) => {
+ipcMain.on('video:submit', (event, path) => {
     console.log('path')
+    ffmpeg.ffprobe(path, (err, metadata) => {
+
+        mainWindow.webContents.send('video:get-metadata', metadata.format.duration);
+    })
 });
